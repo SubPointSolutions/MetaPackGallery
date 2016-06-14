8bf7f6faf8ad76ddba13de8d985d62c470e8a5c7
Maarten Balliauw <maarten@balliauw.be>
2016-06-14 19:34:13 +0200

    Set expiry date in migration to a range between 90 and 110 days

--- src/NuGetGallery/Migrations/201606020741056_CredentialExpires.cs
+++ src/NuGetGallery/Migrations/201606020741056_CredentialExpires.cs
@@ -10,7 +10,8 @@ namespace NuGetGallery.Migrations
             AddColumn("dbo.Credentials", "Created", c => c.DateTime(nullable: false, defaultValueSql: "GETUTCDATE()"));
             AddColumn("dbo.Credentials", "Expires", c => c.DateTime());
 
-            Sql("UPDATE [dbo].[Credentials] SET [Created] =  GETUTCDATE(), [Expires] = DATEADD(Day, 90, GETUTCDATE()) WHERE [Type] = 'apikey.v1'");
+            // Set expiration date to 90 days + a random value between 0 and 20 (-> max. 110 days)
+            Sql("UPDATE [dbo].[Credentials] SET [Created] =  GETUTCDATE(), [Expires] = DATEADD(Day, 90 + ABS(CHECKSUM(NewId())) % 20, GETUTCDATE()) WHERE [Type] = 'apikey.v1'");
         }
         
         public override void Down()
